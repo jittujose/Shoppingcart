c866a6b3d71e5d3ea8dbc77b720a83e5ff89f1f8
Jittu Jose <jittujose99@gmail.com>
2022-01-17 18:23:19 +0530

    updated

--- helpers/product-helpers.js
+++ helpers/product-helpers.js
@@ -1,13 +1,16 @@
+const { resolve, reject } = require('promise')
+
 var db=require('../config/connection')
+
 
 module.exports={
 
     addProduct:(product,callback)=>{
-        product.Price=parseInt(product.Price)
+        product.Price=parseInt(product.Price);
         console.log(product);
 
         db.get().collection('product').insertOne(product).then((data)=>{
-            console.log("######",data);
+            console.log(data);
             
             callback(data)
         })
