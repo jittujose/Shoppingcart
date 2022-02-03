c1e95c84b25eec1bf33d8676455fbb5558c1ac26
Jittu Jose <jittujose99@gmail.com>
2022-02-03 14:09:37 +0530

    partial complete

--- routes/admin.js
+++ routes/admin.js
@@ -1,45 +1,37 @@
+const { response } = require('express');
 var express = require('express');
+const async = require('hbs/lib/async');
 const productHelpers = require('../helpers/product-helpers');
 var router = express.Router();
 var productHelper=require('../helpers/product-helpers')
 
+
+
 /* GET users listing. */
 router.get('/', function(req, res, next) {
+  
 
-  let products=[
-    {
-      name:"IPHONE 11 ",
-      category:'Mobile',
-      description:"This is a good phone",
-      image:"https://kddi-h.assetsadobe3.com/is/image/content/dam/au-com/mobile/mb_img_25.jpg?scl=1"
+  productHelpers.getAllProducts().then((products)=>{
+    console.log(products)
+    res.render('admin/view-products',{admin:true,products})
+  })
 
-    },
-    {
-      name:"IPHONE 10 ",
-      category:'Mobile',
-      description:"This is a Pwolli phone",
-      image:"https://m.media-amazon.com/images/I/61nPiOOv9BL._SL1500_.jpg"
-
-    },
-    {
-      name:"IPHONE 8 ",
-      category:'Mobile',
-      description:"It helps you well",
-      image:"https://m.media-amazon.com/images/I/71w3oJ7aWyL._SX466_.jpg"
-
-    },
-    {
-      name:"IPHONE 10 ",
-      category:'Mobile',
-      description:"This is a Pwolli phone",
-      image:"https://m.media-amazon.com/images/I/61nPiOOv9BL._SL1500_.jpg"
-
-    }
-    
-  ]
-
-  res.render('admin/view-products',{admin:true,products})
+  
 });
+
+router.get('/all-orders',function(req,res,next){
+  productHelpers.getAllOrders().then((orders)=>{
+    console.log(orders);
+    res.render('admin/all-orders',{admin:true,orders})
+  })
+});
+
+router.get('/admin/view-order-products/:id',async(req,res)=>{
+  let products=await productHelpers.getOrderProducts(req.params.id)
+  
+  res.render('admin/view-order-products',{admin:true,products})
+})
+
 router.get('/add-product',function(req,res)
 {
   res.render('admin/add-product')
@@ -49,14 +41,42 @@ router.post('/add-product',(req,res)=>{
   console.log(req.files.Image);
 
   productHelpers.addProduct(req.body,(id)=>{
+    console.log("#####",id);
     let image=req.files.Image
+
     image.mv('./public/product-images/'+id+'.jpg',(err,done)=>{
       if(!err){
-        res.render("admin/add-product")
+        res.render("admin/add-product",{admin:true})
+      }else{
+        console.log(err);
       }
     })
     
   })
 
+})
+router.get('/delete-product/:id',(req,res)=>{
+  let proId=req.params.id
+  console.log(proId);
+  productHelpers.deleteProduct(proId).then((response)=>{
+    res.redirect('/admin/')
+  })
+})
+
+router.get('/edit-product/:id',async (req,res)=>{
+  let product=await productHelpers.getProductDetails(req.params.id)
+  console.log(product);
+  res.render('admin/edit-product',{product})
+})
+router.post('/edit-product/:id',(req,res)=>{
+  let id=req.params.id
+  productHelpers.updateProduct(req.params.id,req.body).then(()=>{
+    res.redirect('/admin')
+    if(req.files.Image){
+      let image=req.files.Image
+      image.mv('./public/product-images/'+id+'.jpg')
+        
+    }
+  })
 })
 module.exports = router;
--- routes/user.js
+++ routes/user.js
@@ -1,40 +1,138 @@
+const { response } = require('express');
 var express = require('express');
+const async = require('hbs/lib/async');
 var router = express.Router();
+const productHelpers = require('../helpers/product-helpers');
+const userHelpers=require('../helpers/user-helpers')
+const verifyLogin=(req,res,next)=>{
+  if(req.session.userLoggedIn){
+    next()
+  }else{
+    res.redirect('/login')
+  }
+}
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  let products=[
-    {
-      name:"IPHONE 11 ",
-      category:'Mobile',
-      description:"This is a good phone",
-      image:"https://kddi-h.assetsadobe3.com/is/image/content/dam/au-com/mobile/mb_img_25.jpg?scl=1"
+router.get('/', async function(req, res, next) {
+  let user=req.session.user
+  console.log(user);
+  let cartCount=null
+  if(req.session.user){
+  cartCount=await userHelpers.getCartCount(req.session.user._id)
+  }
 
-    },
-    {
-      name:"IPHONE 10 ",
-      category:'Mobile',
-      description:"This is a Pwolli phone",
-      image:"https://m.media-amazon.com/images/I/61nPiOOv9BL._SL1500_.jpg"
+  productHelpers.getAllProducts().then((products)=>{
+    
+    res.render('user/view-products',{products,user,cartCount})
+  })
 
-    },
-    {
-      name:"IPHONE 8 ",
-      category:'Mobile',
-      description:"It helps you well",
-      image:"https://m.media-amazon.com/images/I/71w3oJ7aWyL._SX466_.jpg"
+  
+});
+router.get('/login',(req,res)=>{
+  if(req.session.user){
+    res.redirect('/')
+  }else{
+  res.render('user/login',{"loginErr":req.session.userLoginErr})
+  req.session.userLoginErr=false
+  }
+})
+router.get('/signup',(req,res)=>{
+  res.render('user/signup')
+})
+router.post('/signup',(req,res)=>{
+  userHelpers.doSignup(req.body).then((response)=>{
+    console.log(req.body);
+    console.log(response);
+    
+    req.session.user=response
+    req.session.userLoggedIn=true
+    res.redirect('/')
+  })
+})
+router.post('/login',(req,res)=>{
+  userHelpers.doLogin(req.body).then((response)=>{
+    if(response.status){
+      
+      req.session.user=response.user
+      req.session.userLoggedIn=true
+      res.redirect('/')
+    }else{
+      req.session.userLoginErr="Invalid username or password"
+      res.redirect('/login')
+    }
+  })
+})
+router.get('/logout',(req,res)=>{
+  req.session.user=null
+  req.session.userLoggedIn=false
+  res.redirect('/')
+})
+router.get('/cart',verifyLogin,async(req,res)=>{
+  let products=await userHelpers.getCartProducts(req.session.user._id)
+  if(products.length===0){
+    res.render('user/empty-cart',{user:req.session.user})
+  }else{
+  let totalValue= await userHelpers.getTotalAmount(req.session.user._id)
+  console.log(products);
+  res.render('user/cart',{products,user:req.session.user,totalValue})
+}} ) 
 
-    },
-    {
-      name:"IPHONE 10 ",
-      category:'Mobile',
-      description:"This is a Pwolli phone",
-      image:"https://m.media-amazon.com/images/I/61nPiOOv9BL._SL1500_.jpg"
+router.get('/add-to-cart/:id',(req,res)=>{
+  console.log("api call");
+  userHelpers.addToCart(req.params.id,req.session.user._id).then(()=>{
+      res.json({status:true})
+  })
+})
 
+router.post('/change-product-quantity',(req,res,next)=>{
+  userHelpers.changeProductQuantity(req.body).then(async(response)=>{
+    response.total=await userHelpers.getTotalAmount(req.body.user)
+    res.json(response)
+  })
+})
+
+router.post('/remove-cart-product',(req,res,next)=>{
+  userHelpers.removeCartProduct(req.body).then((response)=>{
+        res.json(response)
+  })
+})
+
+router.get('/place-order',verifyLogin,async(req,res)=>{
+  let total=await userHelpers.getTotalAmount(req.session.user._id)
+  res.render('user/place-order',{total,user:req.session.user})
+})
+
+router.post('/place-order',async(req,res)=>{
+  let products=await userHelpers.getCartProductList(req.body.userId)
+  let totalPrice=await userHelpers.getTotalAmount(req.body.userId)
+  userHelpers.placeOrder(req.body,products,totalPrice).then((orderId)=>{
+    if(req.body['payment-method']==='COD'){
+      res.json({status:true})
+    }else{
+      userHelpers.generateRazorpay(orderId,totalPrice).then((response)=>{
+        res.json(response)
+      })
     }
     
-  ]
-  res.render('index', {products,admin:false });
-});
+  })
+  console.log(req.body);
+})
+router.get('/order-success',(req,res)=>{
+  res.render('user/order-success',{user:req.session.user})
+})
+
+router.get('/orders',async(req,res)=>{
+  let orders=await userHelpers.getUserOrders(req.session.user._id)
+  res.render('user/orders',{user:req.session.user,orders})
+})
+
+router.get('/view-order-products/:id',async(req,res)=>{
+  let products=await userHelpers.getOrderProducts(req.params.id)
+  res.render('user/view-order-products',{user:req.session.user,products})
+})
+
+router.post('/verify-payment',(req,res)=>{
+  console.log(req.body);
+})
 
 module.exports = router;
